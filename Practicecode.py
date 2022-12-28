ba53d4b6110af70ae1df223f3eebcec0766d0f03
Vishnu Akash Chandrasekaran <88604788+Vishnu-Akash-Chandrasekaran@users.noreply.github.com>
2022-12-28 09:40:07 +0530

    Add files via upload

---
+++ Practicecode.py
@@ -0,0 +1,10 @@
+print("Hello Vishnu Akash, Welcome to Python training")
+
+name = "Vishnu Akash"
+age = 24
+print()
+print(name,age,sep="*")
+print("Address of name", id(name))
+print("Address of age", id(age))
+print(type(name))
+print(type(age))
---
+++ replacementOperator.py
@@ -0,0 +1,16 @@
+name = "Vishnu Akash"
+city = "Bangalore"
+age = 24
+print(name,age)
+print("name is: ", name)
+print("age is: ",age)
+print("hi {} your age is {}".format(name,age))
+print(f"Welcome {name} to {city}")
+
+#*******************
+# "f" string => it will do expression
+a = 25
+b = 25
+print(f"Addition of {a} and {b} is {a+b}")
+# "f" string => it will do function
+print(f"Datatype of a is {type(a)}")
