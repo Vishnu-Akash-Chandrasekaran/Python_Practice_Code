1b34c42b8ce3078b6f7e71b132f51ad89cbcadf2
Vishnu Akash Chandrasekaran <88604788+Vishnu-Akash-Chandrasekaran@users.noreply.github.com>
2022-12-29 09:57:26 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,17 @@
+if 100 > 99 and\
+200 <= 300 and\
+True != False:
+    print("Hello World!")
+
+l = [1,2,3,4,5,6,7,8,9,10]
+print(l)
+
+print("**************************************")
+
+# This is add function adding two numbers
+
+def add(a,b):
+    t = a+b
+    print(t)
+print("Vishnu Akash")
+add(100,200)
