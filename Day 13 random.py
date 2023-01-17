a6904887f3b249c47fba9c1d1b24cb27c1b4f2e8
Vishnu Akash Chandrasekaran <88604788+Vishnu-Akash-Chandrasekaran@users.noreply.github.com>
2023-01-17 19:35:29 +0530

    Add files via upload

---
+++ Day 13 random.py
@@ -0,0 +1,25 @@
+# random func
+'''import random
+
+n = random.randint(0,100)
+print(n)'''
+
+'''from random import randint
+
+n = randint(0,100)
+print(n)'''
+
+'''from random import *
+
+n = randint(0,100)
+print(n)'''
+'''import random as rm
+
+n = rm.randint(0,100)
+print(n)'''
+
+from custommath import x,add,product
+
+print(x)
+add(10,20)
+product(11,22)
